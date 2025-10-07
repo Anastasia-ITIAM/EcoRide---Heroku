a78da3b4fb5666fa0c80740ab1a0a0baa365747f
Anastasia-ITIAM <anastasiia_degtiar@icloud.com>
2025-10-07 22:20:15 +0200

    Disable MongoDB bundle - use only MySQL

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    // Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true], // Désactivé - migration vers MySQL
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
